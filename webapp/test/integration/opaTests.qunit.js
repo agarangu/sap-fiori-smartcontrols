16bae85419a249a1e2a0d060296ae9472a64f62b
Angel Aranguiz <angel.aranguiz@gmail.com>
2025-03-25 17:24:52 +0000

    Basic project structure

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["org/bootcamp/fiori/smartcontrols/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"org/bootcamp/fiori/smartcontrols/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
